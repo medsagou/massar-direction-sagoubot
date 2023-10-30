e8e4d96660ab916e8c07aff15fc842d9da1067af
Mohamed Sagou <sakou81833@gmail.com>
2023-10-30 01:02:03 +0100

    finshing list reader

--- Read_XLSB_File.py
+++ Read_XLSB_File.py
@@ -4,7 +4,7 @@ from print_sagou import *
 
 
 class Read_Db:
-    def __init__(self, input_file = "db/file_data.xlsb", output = "db/Book1.xlsx", df = ""):
+    def __init__(self, input_file = "db/file_data.xlsb", output_file = "db/Book1.xlsx", df = "", to_fill_by_said = "db/Book10000.xlsx"):
         self.index = {0: "CLASS_StudentIndex",
                       1: "Niveau",
                       2: "class_name",
@@ -13,11 +13,13 @@ class Read_Db:
                       5: "nom",
                       6: "prenom"}
         self.input_file = input_file
-        self.output_file = output
+        self.output_file = output_file
         self.df = df
         self.init_cell = ["A"]
         self.start_col = 'A'
         self.end_col = 'C'
+        self.workbook_output = self.get_workbook(output_file)
+        self.to_fill_by_said = to_fill_by_said
 
     def get_key(self, val):
 
@@ -38,24 +40,23 @@ class Read_Db:
         self.df = df
         return df
 
-    def get_workbook(self):
-        workbook = openpyxl.load_workbook(self.output_file)
+    def get_workbook(self, file_name):
+        workbook = openpyxl.load_workbook(file_name)
         return workbook
 
     def get_workbook_sheet(self, workbook ,sheet):
         return workbook[sheet]
 
-    def add_value_to_sheet(self, workbook, cell, value):
-        worksheet = self.get_workbook_sheet(workbook=workbook, sheet="Sheet4")
+    def add_value_to_sheet(self, worksheet, cell, value):
         cell_to_update = worksheet[cell]
         cell_to_update.value = value
-        workbook.save(self.output_file)
         return
 
 
     def create_copy_sheet(self, class_name = "", workbook = "", source_sheet = ""):
         new_sheet = workbook.copy_worksheet(source_sheet)
         new_sheet.title = class_name
+        new_sheet.sheet_view.rightToLeft = True
         return
 
 
@@ -73,33 +74,81 @@ class Read_Db:
         except ValueError:
             pass
         return L
+    def restart_workbook_output(self):
+        self.workbook_output.close()
+        self.workbook_output = self.get_workbook(self.output_file)
+        return
+
+    def get_sheet_names_workbout_output(self):
+        return self.workbook_output.sheetnames
 
     def create_all_class_sheet(self):
+        class_in_sheet = self.get_sheet_names_workbout_output()
         classes_list = self.get_column_list_from_df(column_key=self.get_key("class_name"))
         workbook = openpyxl.load_workbook(self.output_file)
         source_sheet = workbook["BaseSheet"]
         for classe in classes_list:
+            if classe in class_in_sheet:
+                print_error(f"SHEET FOR {classe} ALREADY EXIST")
+                continue
             print_info(f"CREATE A SHEET FOR {classe} CLASS")
             if classe != "":
                 self.create_copy_sheet(class_name=classe, workbook=workbook, source_sheet = source_sheet)
 
         workbook.save(self.output_file)
-        workbook.close()
+        # workbook.close()
         return
 
     def fill_all_class_sheets(self):
         self.create_all_class_sheet()
+        # already check above
         if str(self.df) == "":
             self.get_data_from_xlsb()
+        print_info("RESTARTING WORKSHEET")
+        self.restart_workbook_output()
+        class_in_sheet = list(self.get_sheet_names_workbout_output())
+        # print(class_in_sheet)
+        for k in range(len(class_in_sheet)):
+            worksheet = self.get_workbook_sheet(workbook = self.workbook_output, sheet=class_in_sheet[k])
+            i = 0
+            print_info(f"WORKING ON {class_in_sheet[k]} CLASS DATA TO SHEET")
+            for row in list(self.df.values):
+                if row[self.get_key("class_name")] == str(class_in_sheet[k]):
+                    i += 1
+                    # print(row)
+                    for col in range(ord(self.start_col), ord(self.end_col) + 1):
+                        if row[0] != 0:
+                            if chr(col) == "A":
+                                self.add_value_to_sheet(worksheet=worksheet, cell=chr(col) + str(9 + i), value=row[self.get_key("student_index")])
+                            elif chr(col) == "B":
+                                self.add_value_to_sheet(worksheet=worksheet, cell=chr(col) + str(9 + i), value=row[self.get_key("CNE")])
+                            elif chr(col) == "C":
+                                self.add_value_to_sheet(worksheet=worksheet, cell=chr(col) + str(9 + i),
+                                                        value=str(row[self.get_key("nom")] + " " + row[self.get_key("prenom")]))
 
-        for row in self.df.values:
-            for col in range(ord(self.start_col), ord(self.end_col) + 1):
-                print(row, chr(col))
+
+                        if i > 49:
+                            return
+                else:
+                    continue
+            self.workbook_output.save(self.to_fill_by_said)
+            # self.workbook_output.close()
+
+
 
         return
-# db = Read_Db()
-# db.get_data_from_xlsb()
-# print(db.get_classes_list_from_df())
-# db.fill_all_class_sheets()
+
+
+
+
+db = Read_Db()
+# db.create_all_class_sheet()
+# add_value_to_sheet(worksheet=db.workbook_output["3ASCG-2"], cell="B11", value = "test")
+# db.workbook_output.save("db/Book13.xlsx")
+# db.workbook_output.close()
+# # db.get_data_from_xlsb()
+# # print(db.get_classes_list_from_df())
+#
+db.fill_all_class_sheets()
 
 
